90de7f794944ce110e02383fc8e0d9f5a24d5f3d
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2025-08-05 00:37:34 +0100

    google font url removed

--- app/components/main/Title.vue
+++ app/components/main/Title.vue
@@ -1,6 +1,6 @@
 <template>
     <div data-aos="fade-up">
-        <h2 class="text-5xl font-bold mb-3">{{ title }}</h2>
+        <h2 class="text-5xl font-bold mb-3 !font-fredaricka">{{ title }}</h2>
         <div class="max-w-[100px] w-full bg-secondary py-0.5"></div>
     </div>
 </template>
@@ -10,10 +10,7 @@ defineProps<{title: string}>()
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Anton+SC&family=Fredericka+the+Great&display=swap');
 h2 {
-font-family: "Fredericka the Great", serif;
- 
   font-style: normal;
 }
 
